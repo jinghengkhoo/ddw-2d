1cf06b295935f219b4f3934015ecac6167e17822
student <team2924806@asn2071802>
2023-11-23 14:04:37 +0000

    fixed forms and removed Werkzeug version requirement

--- app/forms.py
+++ app/forms.py
@@ -11,6 +11,6 @@ class WheatDataEntryForm(FlaskForm):
 	lon = FloatField('longitude', validators=[DataRequired()])
 	lat = FloatField('latitude', validators=[DataRequired()])
 	hdd = IntegerField('Heating Degree Days (Jun - October)', validators=[DataRequired()])
-	tavg = FloatField('Average Temperature (Jun - October)', validators=[DataRequired()])
 	tdiff = FloatField('Temperature Difference (Max - Min) (Jun - October)', validators=[DataRequired()])
+	tavg = FloatField('Average Temperature (Jun - October)', validators=[DataRequired()])
 	run = SubmitField('Run')
